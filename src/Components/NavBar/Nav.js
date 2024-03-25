a07a4d176585ce948f960747a14e229727ec15e1
Adam Jeffery <adamjeff143@gmail.com>
2024-03-25 19:21:44 +0000

    NavBar

---
+++ src/Components/NavBar/Nav.js
@@ -0,0 +1,12 @@
+ export default function Navbar() {
+    return (<>
+        <nav>
+            <div>
+                <form>
+                    <input type="text" placeholder="Search"></input>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </nav>
+    </>)
+ }
